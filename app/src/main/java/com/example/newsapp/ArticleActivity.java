3570c5b128df2f3adbfd077ee15f314f4e35bfae
Pawandeep <sgurminder982@gmail.com>
2021-04-14 03:14:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/ArticleActivity.java
@@ -0,0 +1,25 @@
+package com.example.newsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent intent = getIntent() ;
+
+        webView.loadData(intent.getStringExtra("content"),"text/html","UTF-8");
+    }
+}
